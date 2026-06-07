e4181cc22808ec57c6fe0b13a8677f5555d43593
Umangpandey75 <umangpandey.co@gmail.com>
2026-06-07 18:46:39 +0530

    Configure frontend for Vercel deployment

--- frontend/static/js/main.js
+++ frontend/static/js/main.js
@@ -1,4 +1,6 @@
-const BACKEND_URL = 'http://127.0.0.1:5000';
+const BACKEND_URL = window.location.hostname === '127.0.0.1' || window.location.hostname === 'localhost'
+    ? 'http://127.0.0.1:5000'
+    : '';
 
 // Global variables
 window.csrfToken = '';
